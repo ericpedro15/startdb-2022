1dbdf9890a41d9370add4a1c48906fcd5c1ef974
Eric Anacleto <ericpedro15@hotmail.com>
2022-07-24 22:34:00 -0300

    Update forca.js

--- desafio/forca.js
+++ desafio/forca.js
@@ -1,16 +1,69 @@
 class Forca {
 
-  chutar(letra) { }
+  constructor(palavraSecreta){
+    this._palavraSecreta = palavraSecreta.toLowerCase();
+    this._letrasChutadas = [];
+    this._vidas = 6;
+    this._palavra = []
+    this._estado = 'aguardando chute'
+    this.inicializaPalavra();
+  }
 
-  buscarEstado() { return ""; } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
+  inicializaPalavra(){
+    for (let i = 0; i < this._palavraSecreta.length; i++) {
+      this._palavra[i] = '_';
+    }
+  }
+
+  chutar(letra) {
+    letra = letra.toLowerCase()
+    if(this._palavraSecreta.includes(letra)){
+
+      for(let i = 0; i < this._palavraSecreta.length; i++){
+
+        if(this._palavraSecreta[i] == letra && this._palavra[i] == '_' && letra.length == 1){
+          this._palavra[i] = letra;
+        }
+        
+      }
+      
+    } else {
+      if(!this._letrasChutadas.includes(letra) && letra.length == 1){
+        this._vidas -= 1;
+      }
+    }
+
+    if(!this._letrasChutadas.includes(letra) && letra.length == 1){
+      this._letrasChutadas.push(letra)
+      this._letrasChutadas.sort()
+    }
+
+  }
+
+  buscarEstado() {
+    if(this._vidas > 0 && !this._palavra.includes('_')){
+      this._estado = 'ganhou'
+      return this._estado;
+    }
+
+    if(this._vidas == 0){
+      this._estado = 'perdeu'
+      return this._estado;
+    }
+      this._estado = 'aguardando chute'
+      return this._estado;
+
+
+  } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
 
   buscarDadosDoJogo() {
       return {
-          letrasChutadas: [], // Deve conter todas as letras chutadas
-          vidas: 6, // Quantidade de vidas restantes
-          palavra: [] // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
+          letrasChutadas: this._letrasChutadas, // Deve conter todas as letras chutadas
+          vidas: this._vidas, // Quantidade de vidas restantes
+          palavra: this._palavra // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
       }
   }
+
 }
 
 module.exports = Forca;
